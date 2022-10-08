14f4ff1c274d038b1f6fe2c3f3109b95db295e2f
Originaljs <1154694433@qq.com>
2022-10-08 22:56:15 +0800

    20221008-tj-1

--- src/2d/App.vue
+++ src/2d/App.vue
@@ -1,24 +1,27 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-   
+  <div class="mask">
+    <div class="load" v-show="!isLoad">
+      <a-spin :indicator="indicator" />
+      <span class="text-tip"> 正在加载...</span>
+    </div>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import { LoadingOutlined } from "@ant-design/icons-vue";
+import { defineComponent, h, ref } from "vue";
+const isLoad = ref(false);
+const indicator = h(LoadingOutlined, {
+  style: { fontSize: "54px" },
+  spin: true,
+});
+setTimeout(() => {
+  isLoad.value = true;
+}, 2000);
+</script>
+
+<style lang="less">
+@import "@/2d/css/rootStyle.less";
 </style>
