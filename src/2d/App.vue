47d01048377e4c94dc58388ef2f9e345027be551
Originaljs <1154694433@qq.com>
2022-10-25 00:04:40 +0800

    20221025-demo-1

--- src/2d/App.vue
+++ src/2d/App.vue
@@ -5,18 +5,27 @@
       <span class="text-tip"> 正在加载...</span>
     </div>
     <header class="header">
-      <img class="logo" src="@/2d/assets/img/home/logo.png">
+      <img class="logo" src="@/2d/assets/img/home/logo.png" />
       <h1 class="title">四联测控数字孪生工厂</h1>
-      <div class="time"> {{time}}</div>
+      <div class="time">{{ time }}</div>
     </header>
     <main class="content">
       <router-view></router-view>
     </main>
+    <section class="third-button" v-show="route.path == '/thirdly'">
+      <div :class="['item',clickIndex == index + 1 && animate ?'active':'' ]" v-for="(item, index) in buttonList" :key="index" @click="animationRun(index+1)">
+        {{ item }}
+      </div>
+    </section>
     <footer class="footer">
-      <router-link v-for="item in list" :key="item.path" :to="item.path" :class="['item',route.path==item.path?'active':'']">
-        {{item.name}}
+      <router-link
+        v-for="item in list"
+        :key="item.path"
+        :to="item.path"
+        :class="['item', route.path == item.path ? 'active' : '']"
+      >
+        {{ item.name }}
       </router-link>
-
     </footer>
   </div>
 </template>
@@ -24,10 +33,14 @@
 <script setup lang="ts">
 import { LoadingOutlined } from "@ant-design/icons-vue";
 import { defineComponent, h, ref, shallowRef } from "vue";
-import { formatterDate } from '@/2d/unitls/util'
+import { formatterDate } from "@/2d/unitls/util";
 import { useRoute, useRouter } from "vue-router";
 const route = useRoute();
 const router = useRouter();
+let href = window.location.hash;
+if (href.length > 2) {
+  router.push("/");
+}
 let time = ref(formatterDate("Y年M月D日 星期W h:m"));
 let timmer = setInterval(() => {
   time.value = formatterDate("Y年M月D日 星期W h:m");
@@ -42,27 +55,40 @@ setTimeout(() => {
   isLoad.value = true;
 }, 2000);
 
-const list = shallowRef(
-  [
-    {
-      path: "/",
-      name: "首页",
-    },
-    {
-      path: "/first",
-      name: "一楼",
-    },
-    {
-      path: "/second",
-      name: "二楼",
-    },
-    {
-      path: "/thirdly",
-      name: "三楼",
-    },
-  ]
+const list = shallowRef([
+  {
+    path: "/",
+    name: "首页",
+  },
+  {
+    path: "/first",
+    name: "一楼",
+  },
+  {
+    path: "/second",
+    name: "二楼",
+  },
+  {
+    path: "/thirdly",
+    name: "三楼",
+  },
+]);
+const buttonList = shallowRef(["SMT-I", " SMT-II", "SMT-III", "THT生产线"]);
+const clickIndex = ref(0);
+const animate =ref(false)
+const animationRun = (index:number)=>{
+  if(clickIndex.value ==index){
+    animate.value = !animate.value;
+  }else{
+    clickIndex.value = index
+    animate.value = true
+  }
+  if(index == 4 ){
+    return
+  }else{
 
-)
+  }
+}
 </script>
 
 <style lang="less">
